84be1bb23f4c848091ecb475e1a28bdea006d7be
divyanshucodes20 <155738552+divyanshucodes20@users.noreply.github.com>
2024-10-15 01:27:23 +0530

    Refactor code structure and add fixed header and footer in Layout component

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -4,6 +4,7 @@ import Footer from './components/Footer/footer'
 import { Outlet } from 'react-router-dom'
 function Layout(){
   return (
+    // outlet header aur footer fixed rahege
    <>
    <Header/>
    <Outlet/>
