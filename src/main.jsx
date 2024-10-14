314a558c31509ae2942f08ffdbd70b91a3f24a5f
divyanshucodes20 <155738552+divyanshucodes20@users.noreply.github.com>
2024-10-14 20:48:13 +0530

    Refactor code structure and dependencies
    - Remove unused App component
    - Add Layout component for page layout
    - Update package.json to include react-router-dom
    - Update main.jsx to use react-router-dom for routing
    - Delete unused vite.svg file
    - Add About component for About page
    - Update package-lock.json to include react-router-dom
    - Add Home component for Home page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
 import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout'
+import Home from './components/Home/home'
+import About from './components/About/About'
+import Contact from './components/Contact/contactus'
+const router=createBrowserRouter([
+  {
+    path:'/',
+    element:<Layout/>,
+    children:[
+      {
+        path:"",
+        element:<Home/>
+      },
+      {
+        path:"about",
+        element:<About/>
+      },
+      {
+        path:"contact",
+        element:"<Contact/>"
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
